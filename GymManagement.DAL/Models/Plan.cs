c1e56af9010f91cd1882eb7c5d697c4925c59b00
Mohamed Ramadan <mr3715708@gmail.com>
2026-06-08 02:27:45 +0300

    Add Models

--- GymManagement.DAL/Models/Plan.cs
+++ GymManagement.DAL/Models/Plan.cs
@@ -1,15 +1,14 @@
-﻿namespace GymManagement.Models
+﻿using GymManagement.DAL.Models;
+
+namespace GymManagement.Models
 {
-    public class Plan
+    public class Plan : BaseEntity
     {
-        public int Id { get; set; }
         public string Name { get; set; } = default!;
         public string Description { get; set; } = default!;
         public decimal Price { get; set; }
         public int DurationDays { get; set; }
         public bool IsActive { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? UpdatedAt { get; set; }
 
     }
 }
---
+++ GymManagement.DAL/Models/Trainer.cs
@@ -0,0 +1,16 @@
+﻿using GymManagement.DAL.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagement.DAL.Models
+{
+    public class Trainer : GymUser
+    {
+        public Specialty Specialty { get; set; }
+        
+        //HireDate = CreatedAt in BaseEntity
+    }
+}
